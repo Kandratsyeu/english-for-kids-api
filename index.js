d2fbce4d016cf60a84d3526ac8caf9355419f843
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-14 00:00:56 +0300

    db test

--- index.js
+++ index.js
@@ -19,15 +19,10 @@ app.get('/db', async (req, res) => {
 
   client.connect()
 
-  client.query(
-    'SELECT table_schema,table_name FROM information_schema.tables;',
-    (err, res) => {
-      if (err) throw err
-      for (let row of res.rows) {
-        console.log(JSON.stringify(row))
-      }
-      client.end()
-    }
+  res.end(
+    client.query(
+      'SELECT table_schema,table_name FROM information_schema.tables WHERE table_name = "columns";'
+    )
   )
 })
 

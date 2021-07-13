f9eccb1d7546628c5239ba89d76e757c6998982b
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-13 21:37:31 +0300

    db test

--- index.js
+++ index.js
@@ -5,26 +5,23 @@ const app = express()
 
 const PORT = process.env.PORT || 80
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
-
 client.connect()
 
 app.get('/', (req, res) => {
-  client.query(
-    'SELECT table_schema,table_name FROM information_schema.tables;',
-    (err, res) => {
-      if (err) throw err
-      for (let row of res.rows) {
-        console.log(JSON.stringify(row))
-      }
-      client.end()
-    }
+  res.end = '<h1>hi</h1>'
+})
+
+app.get('/db', async (req, res) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+  res.end = client.query(
+    'SELECT table_schema,table_name FROM information_schema.tables;'
   )
+  client.end()
 })
 
 app.listen(PORT, () => {

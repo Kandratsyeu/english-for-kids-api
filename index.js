3d005d8cb4bc5b3647a93fbd4d8a96f1955bde86
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-13 21:04:44 +0300

    db test

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const { Client } = require('pg')
 
 const app = express()
 
 const PORT = process.env.PORT || 80
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+client.connect()
+
 app.get('/', (req, res) => {
-  res.json({
-    1: 23,
-  })
+  client.query(
+    'SELECT table_schema,table_name FROM information_schema.tables;',
+    (err, res) => {
+      if (err) throw err
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row))
+      }
+      client.end()
+    }
+  )
 })
 
 app.listen(PORT, () => {

677e2dd33dbfe1b366cb40d645b7be59b7b9f118
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-14 16:49:22 +0300

    db test

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const { Pool, Client } = require('pg')
+const { Client } = require('pg')
 
 const client = new Client({
-  connectionString:
-    'dbname=deb1k8lde6qm9s host=ec2-3-224-7-166.compute-1.amazonaws.com port=5432 user=zbkkqbkrgwzmjh password=d7841d07398ea1b553712cb493eb8dc6450995ea701f4ec0ba864cf0ea581332 sslmode=require',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 client.connect()
-client
-  .query('SELECT table_schema,table_name FROM information_schema.tables;')
-  .then((result) => console.log(result))
-  .catch((e) => console.error(e.stack))
-  .then(() => client.end())
+
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row))
+    }
+    client.end()
+  }
+)

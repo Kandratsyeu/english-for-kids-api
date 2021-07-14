8ae56ccfe1f71ff6659d67aae302508bcbbaa2f0
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-14 16:40:14 +0300

    db test

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const express = require('express')
-const { Client } = require('pg')
-
-const app = express()
-
-const PORT = process.env.PORT || 80
+const { Pool, Client } = require('pg')
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString:
+    'dbname=deb1k8lde6qm9s host=ec2-3-224-7-166.compute-1.amazonaws.com port=5432 user=zbkkqbkrgwzmjh password=d7841d07398ea1b553712cb493eb8dc6450995ea701f4ec0ba864cf0ea581332 sslmode=require',
 })
 
-app.get('/', (req, res) => {
-  res.end('<h1>hi</h1>')
-})
-
-app.get('/db', async (req, res) => {
-  client.connect()
-
-  client
-    .query(
-      'SELECT table_schema,table_name FROM information_schema.tables WHERE table_name = "columns";'
-    )
-    .then((answer) => {
-      res.end(answer)
-      client.end()
-    })
-})
-
-app.listen(PORT, () => {
-  console.log('Server has been started.')
-})
+client.connect()
+client
+  .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  .then((result) => console.log(result))
+  .catch((e) => console.error(e.stack))
+  .then(() => client.end())

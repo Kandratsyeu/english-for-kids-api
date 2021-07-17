a4eb109cf63c55f5538b45916d57eb9e81ed7001
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-17 14:05:50 +0300

    test

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import categoriesRouter from './router'
 
 const PORT = process.env.PORT || 80
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/', categoriesRouter)

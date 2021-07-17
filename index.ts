7256c4c647bc5924f819f5d8d838cc5fb8721040
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-07-17 11:18:49 +0300

    feat: change port

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import cors from 'cors'
 import { json } from 'body-parser'
 import router from './router'
 
+const PORT = Number(process.env.PORT) | 80
+
 const app = express()
 app.use(json())
 app.use(cors())
 
 app.use('/categories', router.categoriesRouter)
 
-app.listen(3000, () => console.log('Server has been started...'))
+app.listen(PORT, () => console.log('Server has been started...'))
